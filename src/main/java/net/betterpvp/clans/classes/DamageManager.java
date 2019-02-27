3ef7a51a2cf87a4f06e4adff916aafe2ea037db4
Tom <tomhoogstra@live.com.au>
2019-02-28 03:22:15 +1100

    Removed old Recall skill

--- src/main/java/net/betterpvp/clans/classes/DamageManager.java
+++ src/main/java/net/betterpvp/clans/classes/DamageManager.java
@@ -10,7 +10,6 @@ import net.betterpvp.clans.client.Client;
 import net.betterpvp.clans.client.ClientUtilities;
 import net.betterpvp.clans.gamer.Gamer;
 import net.betterpvp.clans.gamer.GamerManager;
-import net.betterpvp.clans.skills.selector.skills.assassin.Mirage;
 import net.betterpvp.clans.weapon.WeaponManager;
 import net.betterpvp.core.client.ClientUtilities;
 import net.betterpvp.core.framework.UpdateEvent;
