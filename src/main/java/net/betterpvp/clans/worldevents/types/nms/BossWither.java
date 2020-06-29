47213ba442b9b130b87e6577ff56069b30a11a50
Tom <tomhoogstra@live.com.au>
2020-06-29 23:33:50 +1000

    REWORKED Charles Witherton boss

--- src/main/java/net/betterpvp/clans/worldevents/types/nms/BossWither.java
+++ src/main/java/net/betterpvp/clans/worldevents/types/nms/BossWither.java
@@ -12,9 +12,10 @@ public class BossWither extends EntityWither {
 
         this.targetSelector.a(1, new PathfinderGoalHurtByTarget(this,
                 new Class[0]));
-        this.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget<EntityHuman>(this,
+        this.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget<>(this,
                 EntityHuman.class, true));
 
+        this.goalSelector.a(6, new PathfinderGoalLookAtPlayer(this, EntityHuman.class, 8.0F));
 
     }
 
@@ -24,11 +25,26 @@ public class BossWither extends EntityWither {
 
     }
 
+    @Override
+    protected void initPathfinder(){
+
+    }
+
 
     @Override
     public void a(EntityLiving entityliving, float f) {
 
     }
+
+    public void setMot(Vec3D vec3d){return;}
+
+
+    @Override
+    public void f(double d0, double d1, double d2){return;}
+
+
+    @Override
+    public void h(double d0, double d1, double d2) { return;}
 
 
     public Wither spawn(Location loc) {
