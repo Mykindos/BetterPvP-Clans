b3e98d51081419dc6573fe677df895fcd54858c5
Tom Hoogstra <tomhoogstra@live.com.au>
2020-09-24 20:24:32 +1000

    CHANGE Blink & Flash so you cannot use them while standing in the same block as a gate

--- src/main/java/net/betterpvp/clans/skills/selector/skills/assassin/Flash.java
+++ src/main/java/net/betterpvp/clans/skills/selector/skills/assassin/Flash.java
@@ -212,6 +212,11 @@ public class Flash extends Skill implements InteractSkill {
             return false;
         }
 
+        if (player.getLocation().getBlock().getType().name().contains("GATE")
+                || player.getLocation().add(0, 1, 0).getBlock().getType().name().contains("GATE")) {
+            return false;
+        }
+
         if (player.getTargetBlock((Set<Material>) null, 4).getType() == Material.GLASS) {
             return false;
         }
